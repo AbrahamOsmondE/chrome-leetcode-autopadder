39abd9b4923437d70d5e1d750aebd7a6f058aa9d
abrahamosmonde <abraham.osmond@gmail.com>
2023-08-08 01:16:40 +0800

    finish extension scraping and file formatting

---
+++ src/scripts/content.ts
@@ -0,0 +1,87 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+const values = new Map();
+const axios = require('axios');
+
+const handleButtonClick = () => {
+  const codeEditorElement = document.querySelector('[data-track-load="code_editor"]');
+  if (codeEditorElement) {
+    const linesContentElement = codeEditorElement!.querySelector('.lines-content') as HTMLElement;
+    values.set("submit", linesContentElement.innerText)
+
+    setTimeout(() => {
+      const submissionResult = document.querySelector('[data-e2e-locator="submission-result"]') as HTMLElement;
+      if (submissionResult.innerText === "Accepted") {
+        axios.post()
+      }
+
+    }, 5000)
+  }
+}
+
+setTimeout(() => {
+  const submitButton = document.querySelector('[data-e2e-locator="console-submit-button"]') as HTMLElement;
+  const problemDescription = document.querySelector('[data-track-load="description_content"]') as HTMLElement;
+  const codeEditorElement = document.querySelector('[data-track-load="code_editor"]')as HTMLElement ;
+  const elementsWithClass = document.querySelectorAll('div.flex.space-x-4');
+  const difficulty = document.querySelector('.text-yellow, .text-pink, .text-olive') as HTMLElement;
+  const problemTitle = elementsWithClass[1] as HTMLElement;
+  const currentUrl = window.location.href;
+  
+  const descriptionFormatted = problemDescription.innerHTML.replace(/<p>&nbsp;<\/p>/g, "")
+
+  values.set('readme', '## [' + problemTitle.innerText + '](' + currentUrl + ')\n' + '## ' + difficulty.innerText + '\n' + descriptionFormatted)
+
+
+  if (codeEditorElement) {
+    const linesContentElement = codeEditorElement!.querySelector('.lines-content') as HTMLElement;
+    console.log(linesContentElement.innerText)
+  }
+  
+  if (submitButton) {
+    submitButton.addEventListener('click', handleButtonClick)
+  }
+}, 2000)
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+// if (article) {
+//   const text = article.textContent;
+//   /**
+//    * Regular expression to find all "words" in a string.
+//    *
+//    * Here, a "word" is a sequence of one or more non-whitespace characters in a row. We don't use the
+//    * regular expression character class "\w" to match against "word characters" because it only
+//    * matches against the Latin alphabet. Instead, we match against any sequence of characters that
+//    * *are not* a whitespace characters. See the below link for more information.
+//    *
+//    * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+//    */
+//   const wordMatchRegExp = /[^\s]+/g;
+//   const words = text!.matchAll(wordMatchRegExp);
+//   // matchAll returns an iterator, convert to array to get word count
+//   const wordCount = [...words].length;
+//   const readingTime = Math.round(wordCount / 200);
+//   const badge = document.createElement('p');
+//   // Use the same styling as the publish information in an article's header
+//   badge.classList.add('color-secondary-text', 'type--caption');
+//   badge.textContent = `⏱️ ${readingTime} min read`;
+
+//   // Support for API reference docs
+//   const heading = article.querySelector('h1');
+//   // Support for article docs with date
+//   const date = article.querySelector('time')?.parentNode as HTMLHeadingElement;
+
+//   // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
+//   (date ?? heading)!.insertAdjacentElement('afterend', badge);
+// }
---
+++ webpack/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+   mode: "production",
+   entry: {
+      content: path.resolve(__dirname, "..", "src", "scripts" ,"content.ts"),
+      popup: path.resolve(__dirname, "..", "src", "popup", "popup.ts")
+   },
+   output: {
+      path: path.join(__dirname, "../dist"),
+      filename: "[name].js",
+   },
+   resolve: {
+      extensions: [".ts", ".js"],
+   },
+   module: {
+      rules: [
+         {
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+         },
+      ],
+   },
+   plugins: [
+      new CopyPlugin({
+         patterns: [{from: ".", to: ".", context: "public"}]
+      }),
+   ],
+};
